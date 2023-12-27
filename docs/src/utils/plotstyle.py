8c612d7e8b9f8aac875c71c97b4b79398f7944d8
PascalGitz <pascal.gitz@hotmail.ch>
2023-12-27 23:14:33 +0100

    versuche finish

--- docs/src/utils/plotstyle.py
+++ docs/src/utils/plotstyle.py
@@ -8,10 +8,10 @@ def set_engineering_style():
     plt.rcParams['lines.linewidth'] = 0.7  # Linienstärke
     
     plt.rcParams['axes.grid'] = True  # Gitterlinien im Plot
-    plt.rcParams['grid.color'] = 'black'  # Grid Farbe
-    plt.rcParams['grid.alpha'] = '0.5'  
-    plt.rcParams['grid.linestyle'] = 'dotted'  
-    plt.rcParams['grid.linewidth'] = 0.3  
+    plt.rcParams['grid.color'] = 'lightgray'  # Grid Farbe
+    plt.rcParams['grid.alpha'] = '1'  
+    # plt.rcParams['grid.linestyle'] = 'dotted'  
+    plt.rcParams['grid.linewidth'] = 0.2  # Gitterlinienstärke  
     
     
     plt.rcParams['axes.labelsize'] = 12  # Größe der Achsenbeschriftungen
@@ -33,7 +33,7 @@ def set_engineering_style():
     plt.rcParams['xtick.labelsize'] = 10  # Größe der x-Achsenbeschriftungen
     plt.rcParams['ytick.labelsize'] = 10  # Größe der y-Achsenbeschriftungen
     plt.rcParams['legend.fontsize'] = 10  # Größe der Legende
-    plt.rcParams['figure.figsize'] = (6.5, 3.5)  # Größe der Figur 6.5 Zoll entspricht der Standardbreite des scrbook latex types
+    plt.rcParams['figure.figsize'] = (6.5, 3.)  # Größe der Figur 6.5 Zoll entspricht der Standardbreite des scrbook latex types
     plt.rcParams.update({
         "font.family": "serif",
         "font.serif": ["Palatino Linotype"],
--- docs/src/utils/svg_convert.py
+++ docs/src/utils/svg_convert.py
@@ -1,2 +1,3 @@
 from sympycalcs import pdf_to_svg
 pdf_to_svg("images", delete_original=True)
+pdf_to_svg("images/nino", delete_original=False)
